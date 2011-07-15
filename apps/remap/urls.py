cc52148ec5ef40f84de8945274a493aab47c356e
renooble_hannes <renooble_hannes@mybox.(none)>
2011-07-15 05:33:42 +0200

    Integration of a test of remap in the current Pinax setup
    Created a simple form to receive the location of a potential new
    project, create the basic urls.py and views.py, as well as the
    template html files. According to the pinax tutorial at
    http://www.paolocorti.net/2009/11/29/the-pinax-tutorial-2-developing-the-basic-application-and-plugging-it-in-pinax/
    I created a base.html. Now, base.html extends site_base.html and
    each individual template, e.g. templates/remap/add_project_details.html
    extends base.html.
    
    Commit not test!

--- apps/remap/urls.py
+++ apps/remap/urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns("",
     # url(r"^user_project/(?P<username>\w+)/$", "remap.views.user_projects", name="user_projects"),
 
     # CRUD urls
-    # url(r"^add/$", "remap.views.add_project", name="add_project_location"),
-    # url(r"^add/details/$", "remap.views.add_project_details", name="add_project_details"),
+    url(r"^add/$", "remap.views.add_project", name="add_project_location"),
+    url(r"^add/details/$", "remap.views.add_project_details", name="add_project_details"),
     # url(r"^update/(\d+)/$", "remap.views.update_project", name="update_project"),
     # url(r"^delete/(\d+)/$", "remap.views.delete_project", name="delete_project"),
 )
--- apps/remap/views.py
+++ apps/remap/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from remap.models import Project
+from remap.forms import ReMapLocationForm, ReMapProjectForm, ReMapUserEditProject
+
+def projects(request):
+#
+# Insert code for reading the database with the projects
+# and adding it to the google map
+#
+    return redirect('http://www.django.org')
+
+def add_project(request):
+    locationForm = ReMapLocationForm
+    return render_to_response('add_project_location.html', {
+        'locationForm': locationForm
+    })
+
+def add_project_details(request):
+    projectForm = ReMapProjectForm
+    return render_to_response('add_project_details.html', {
+        'projectForm': projectForm
+    })
+
+
+
+
