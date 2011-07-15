71a8897b2855a50c5d87115b86c2e05c492692f0
renooble_hannes <renooble_hannes@mybox.(none)>
2011-07-15 04:01:34 +0200

    Adjustment of the remap forms.py
    the 'forms.py' were earlier copied from the energy2go server at AD.
    This file contained the Django FormWizard setup.
    For renooble on AD, the FormWizard will be replaced by a simple
    form with the preview form function of Django forms. The earlier
    geoCoding tool is not needed any more since we can use the jQuery app
    for asyncronous geocoding on the site.

--- apps/remap/forms.py
+++ apps/remap/forms.py
@@ -10,70 +10,20 @@ from django.core.mail import send_mail
 
 # from api.fields import ModelAutoCompleteField
 import datetime
-import hashlib 
+# import hashlib 
 
-from reMap.models import Project
-from reTech.models import Windturbine
-from geoCodeAPI import getLocation
+from remap.models import Project
 
-class ReMapNewRawAddressForm(ModelForm):
+class ReMapLocationForm(ModelForm):
     class Meta:
         model = Project
-        fields = ('rawAddress', 'technologyType')
+        fields = ('cleanAddress', 'city', 'state', 'country', 'lat', 'lng')
 
-class ReMapConfirmAddressSelectionForm(forms.Form):
-   locations = forms.CharField()
-
-class ReMapMandatoryForm(ModelForm):
+class ReMapProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ('rawTechnology', 'installationDate', 'installedPower', 'AEP', 'hideLocation')
-        widgets = {'installationDate': SelectDateWidget(years=range(1970, 2011))}
-
-class ReMapAdditionalProjectInfoForm(ModelForm):
-    class Meta:
-        model = Project
-        fields = ('projectBuilding', 'surroundings', 'gridConnection', 'commercialInstallation')
-
-class ReMapUserInformationForm(ModelForm):
-    class Meta:
-        model = Project
-        fields = ('userEmail', 'userComments')
-        widgets = {'userComments': forms.Textarea}
-
-class reMapWizard(FormWizard):
-
-    def render_template(self, request, form, previous_fields, step, context=None):
-        if step == 1: 
-            location = request.POST.get('0-rawAddress')
-            data = getLocation(location)
-
-            # s = SessionStore()
-            # s['locations'] = data
-            # s.save()
-
-            form.fields['locations'] = forms.ChoiceField(widget=RadioSelect(), choices = [])
-            form.fields['locations'].choices = [(data[i]['formatted_address'], data[i]['formatted_address']) for i in range(len(data))]
-        return super(reMapWizard, self).render_template(request, form, previous_fields, step, context)
-
-    def done(self, request, form_list):
-        # request.session['_ReMapFormList'] = form_list
-        instance = Project()
-        for form in form_list:
-            for field, value in form.cleaned_data.iteritems():
-                setattr(instance, field, value)
-        data = getLocation(instance.locations)
-        instance.lat = str(data[0]['lat'])
-        instance.lng = str(data[0]['lng'])
-        instance.country = str(data[0]['country'])
-        instance.cleanAddress= str(data[0]['formatted_address'])
-        instance.entryDate = datetime.datetime.now()
-        instance.activationKey = hashlib.md5(str(data[0]['lat']) + str(data[0]['lng']) + str(data[0]['formatted_address']) + str(instance.entryDate)).hexdigest()[:30]
-        instance.save()
-        LINK_DOMAIN = '127.0.0.1:8000'
-        send_mail('Activate your Project', 'Here is the message. Click here: http://' + LINK_DOMAIN + '/renooble/reMap/activate/' + str(instance.activationKey) + '/', 'hannes@renooble.net', [str(instance.userEmail)], fail_silently=False)
-
-        return HttpResponseRedirect("http://www.google.com")
+        fields = ('rawTechnology', 'installationDate', 'installedPower', 'AEP', 'hideLocation', 'projectBuilding', 'surroundings', 'gridConnection', commercialInstallation', 'userEmail', userComments')
+        widgets = {'installationDate': SelectDateWidget(years=range(1970, 2011)), 'userComments': forms.Textarea}
 
 class ReMapUserEditProject(ModelForm):
     class Meta:
