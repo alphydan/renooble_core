e9f5dd5491a0d2472f5706caf888a5db2336e50e
Hannes <hannes@renooble.net>
2011-08-21 09:45:55 +0200

    updated form

--- apps/about/urls.py
+++ apps/about/urls.py
@@ -5,7 +5,8 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {"template": "about/about.html"}, name="about"),
     url(r"^support_us/$", direct_to_template, {"template": "about/support_us.html"}, name="support_us"),
-    url(r"^privacy/$", direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
-    url(r"^dmca/$", direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
+    url(r"^contact/$", direct_to_template, {"template": "about/contact.html"}, name="contact"),
+    url(r"^contact/thanks/$", direct_to_template, {"template": "about/contact_thanks.html"}, name="contact_thanks"),
+    url(r"^sitemap/$", direct_to_template, {"template": "about/sitemap.html"}, name="sitemap"),
     url(r"^what_next/$", direct_to_template, {"template": "about/what_next.html"}, name="what_next"),
 )
